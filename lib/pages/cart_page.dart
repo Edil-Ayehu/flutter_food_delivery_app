949ec2db02fe25fc16bd73b11dec405f8e964676
Edil-Ayehu <125575950+Edil-Ayehu@users.noreply.github.com>
2024-03-26 07:46:18 +0300

    UI completed!

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_food_delivery_app/components/my_button.dart';
 import 'package:flutter_food_delivery_app/components/my_cart_tile.dart';
 import 'package:flutter_food_delivery_app/models/restaurant.dart';
+import 'package:flutter_food_delivery_app/pages/payment_page.dart';
+import 'package:get/get.dart';
 import 'package:provider/provider.dart';
 
 class CartPage extends StatelessWidget {
@@ -22,34 +24,34 @@ class CartPage extends StatelessWidget {
           foregroundColor: Theme.of(context).colorScheme.inversePrimary,
           actions: [
             // clear cart button
-            if(userCart.isNotEmpty)
-            IconButton(
-              onPressed: () {
-                showDialog(
-                    context: context,
-                    builder: (context) => AlertDialog(
-                          title: const Text(
-                              'Are you sure you want to clear the cart?'),
-                          actions: [
-                            // cancel button
-                            TextButton(
-                              onPressed: () => Navigator.pop(context),
-                              child: const Text('Cancle'),
-                            ),
+            if (userCart.isNotEmpty)
+              IconButton(
+                onPressed: () {
+                  showDialog(
+                      context: context,
+                      builder: (context) => AlertDialog(
+                            title: const Text(
+                                'Are you sure you want to clear the cart?'),
+                            actions: [
+                              // cancel button
+                              TextButton(
+                                onPressed: () => Navigator.pop(context),
+                                child: const Text('Cancle'),
+                              ),
 
-                            // yes button
-                            TextButton(
-                              onPressed: () {
-                                Navigator.pop(context);
-                                restaurant.clearCart();
-                              },
-                              child: const Text('Yes'),
-                            ),
-                          ],
-                        ));
-              },
-              icon: const Icon(Icons.delete),
-            ),
+                              // yes button
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                  restaurant.clearCart();
+                                },
+                                child: const Text('Yes'),
+                              ),
+                            ],
+                          ));
+                },
+                icon: const Icon(Icons.delete),
+              ),
           ],
         ),
         body: Column(
@@ -57,28 +59,30 @@ class CartPage extends StatelessWidget {
             userCart.isEmpty
                 ? const Expanded(child: Center(child: Text("Cart is Empty...")))
                 : Expanded(
-                  child: Column(
-                    children: [
-                      Expanded(
-                       
+                    child: Column(
+                      children: [
+                        Expanded(
                           child: ListView.builder(
                               itemCount: userCart.length,
                               itemBuilder: (context, index) {
                                 // get individual cart item
                                 final cartItem = userCart[index];
-                      
+
                                 // return cart tile UI
                                 return MyCartTile(cartItem: cartItem);
                               }),
                         ),
-                  
+
                         // button to pay
-                        const MyButton(text: 'Checkout'),
+                        MyButton(
+                          text: 'Checkout',
+                          onTap: () => Get.to(() => const PaymentPage()),
+                        ),
 
                         const SizedBox(height: 25),
-                    ],
+                      ],
+                    ),
                   ),
-                ),
           ],
         ),
       );
---
+++ lib/pages/payment_page.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_credit_card/flutter_credit_card.dart';
+import 'package:flutter_food_delivery_app/components/my_button.dart';
+import 'package:get/get.dart';
+
+import 'delivery_progress_page.dart';
+
+class PaymentPage extends StatefulWidget {
+  const PaymentPage({super.key});
+
+  @override
+  State<PaymentPage> createState() => _PaymentPageState();
+}
+
+class _PaymentPageState extends State<PaymentPage> {
+  GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  String cardNumber = '';
+  String expiryDate = '';
+  String cardHolderName = '';
+  String cvvCode = '';
+  bool isCvvFocused = false;
+
+  // user wants to pay
+  void userTappedPay() {
+    if (formKey.currentState!.validate()) {
+      // only show dialog if form is valid
+      showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: const Text('Confirm payment'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: [
+                Text('Card Number: $cardNumber'),
+                Text('Expiry Date: $expiryDate'),
+                Text('Card Holder Name: $cardHolderName'),
+                Text('CVV: $cvvCode'),
+              ],
+            ),
+          ),
+          actions: [
+            // Cancel button
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: const Text('Cancel'),
+            ),
+
+            // yes button
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+                Get.to(() => const DeliveryProgressPage());
+              },
+              child: const Text('Yes'),
+            ),
+          ],
+        ),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        foregroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text("Checkout"),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          // credit card
+          CreditCardWidget(
+            cardNumber: cardNumber,
+            expiryDate: expiryDate,
+            cardHolderName: cardHolderName,
+            cvvCode: cvvCode,
+            showBackView: isCvvFocused,
+            onCreditCardWidgetChange: (value) {},
+          ),
+      
+          // credit card form
+          CreditCardForm(
+            cardNumber: cardNumber,
+            expiryDate: expiryDate,
+            cardHolderName: cardHolderName,
+            cvvCode: cvvCode,
+            onCreditCardModelChange: (data) {
+              setState(() {
+                cardNumber = data.cardNumber;
+                expiryDate = data.expiryDate;
+                cardHolderName = data.cardHolderName;
+                cvvCode = data.cvvCode;
+              });
+            },
+            formKey: formKey,
+          ),
+      
+          const Spacer(),
+      
+          // Pay now button
+          MyButton(text: 'Pay Now', onTap: userTappedPay),
+      
+          const SizedBox(height: 25),
+        ],
+      ),
+    );
+  }
+}
